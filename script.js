94172fb6d7fcef5d4345b471dcea82db3f0eb7f9
Re-aH <104552662+Re-aH@users.noreply.github.com>
2022-08-30 14:55:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -82,6 +82,20 @@ Tone.Transport.scheduleRepeat((time) => {
 }, "4n");
 
 
+window.addEventListener('DOMContentLoaded', function () {
+
+    // create empty buffer
+    var buffer = myContext.createBuffer(1, 1, 22050);
+    var source = myContext.createBufferSource();
+    source.buffer = buffer;
+
+    // connect to output (your speakers)
+    source.connect(myContext.destination);
+
+    // play the file
+    source.noteOn(0);
+
+}, false);
 //testing
 
 

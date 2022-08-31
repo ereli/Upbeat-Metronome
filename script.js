92ce07b2d4e48f1366b0b57f615409d2d92b714b
Re-aH <104552662+Re-aH@users.noreply.github.com>
2022-08-31 18:47:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 'use strict'
 
-const soundEffect = new Audio();
-soundEffect.autoplay = true;
-
-// onClick of first interaction on page before I need the sounds
-// (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
-soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
-
-
-
-
 let bpm = 60
 updateToneBpm()
 const bpmTopLimit = 300
@@ -55,7 +45,6 @@ const togglePlayStop = function () {
 }
 
 playStopBtn.addEventListener('click', function () {
-    soundEffect.play()
     pressPlay()
     updateAnimationTempo()
 })
@@ -96,3 +85,84 @@ Tone.Transport.scheduleRepeat((time) => {
 
 
 
+//try
+
+(function () {
+
+    // Check if the browser supports web audio. Safari wants a prefix.
+    if ('AudioContext' in window || 'webkitAudioContext' in window) {
+
+        //////////////////////////////////////////////////
+        // Here's the part for just playing an audio file.
+        //////////////////////////////////////////////////
+        var play = function play(audioBuffer) {
+            var source = context.createBufferSource();
+            source.buffer = audioBuffer;
+            source.connect(context.destination);
+            source.start();
+        };
+
+        var URL = 'click2.mp3';
+        var AudioContext = window.AudioContext || window.webkitAudioContext;
+        var context = new AudioContext(); // Make it crossbrowser
+        var gainNode = context.createGain();
+        gainNode.gain.value = 1; // set volume to 100%
+        // var playButton = document.querySelector('#play');
+        var yodelBuffer = void 0;
+
+        // The Promise-based syntax for BaseAudioContext.decodeAudioData() is not supported in Safari(Webkit).
+        window.fetch(URL)
+            .then(response => response.arrayBuffer())
+            .then(arrayBuffer => context.decodeAudioData(arrayBuffer,
+                audioBuffer => {
+                    yodelBuffer = audioBuffer;
+                },
+                error =>
+                    console.error(error)
+            ))
+
+        playStopBtn.onclick = function () {
+            return play(yodelBuffer);
+        };
+
+        // Play the file every 2 seconds. You won't hear it in iOS until the audio context is unlocked.
+        // window.setInterval(function () {
+        //     play(yodelBuffer);
+        // }, 5000);
+
+
+        //////////////////////////////////////////////////
+        // Here's the part for unlocking the audio context, probably for iOS only
+        //////////////////////////////////////////////////
+
+        // From https://paulbakaus.com/tutorials/html5/web-audio-on-ios/
+        // "The only way to unmute the Web Audio context is to call noteOn() right after a user interaction. This can be a click or any of the touch events (AFAIK – I only tested click and touchstart)."
+
+        // var unmute = document.getElementById('unmute');
+        playStopBtn.addEventListener('click', unlock);
+
+        function unlock() {
+            console.log("unlocking")
+            // create empty buffer and play it
+            var buffer = context.createBuffer(1, 1, 22050);
+            var source = context.createBufferSource();
+            source.buffer = buffer;
+            source.connect(context.destination);
+
+            // play the file. noteOn is the older version of start()
+            source.start ? source.start(0) : source.noteOn(0);
+
+            // by checking the play state after some time, we know if we're really unlocked
+            setTimeout(function () {
+                if ((source.playbackState === source.PLAYING_STATE || source.playbackState === source.FINISHED_STATE)) {
+                    // Hide the unmute button if the context is unlocked.
+                    // unmute.style.display = "none";
+                }
+            }, 0);
+        }
+
+        // Try to unlock, so the unmute is hidden when not necessary (in most browsers).
+        unlock();
+    }
+}
+)();
